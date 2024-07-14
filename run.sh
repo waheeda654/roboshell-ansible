ebcd1594070e046461716a0b1cfbdc237a78b7ed
Waheeda <waheeda.ias@gmail.com>
2024-07-14 18:54:43 +0530

    new files

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1.wdevops.fun, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1  main.yml
+ansible-playbook -i $1.dev.wdevops.fun, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1  main.yml
