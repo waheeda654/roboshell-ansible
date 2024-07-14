fcc64d70754f761dcf2257f7ced4a85047b529f1
Waheeda <waheeda.ias@gmail.com>
2024-07-14 19:05:59 +0530

    mongo

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-git pull
-ansible-playbook -i $1.dev.wdevops.fun, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1  main.yml
+frontend:
+  git pull
+  ansible-playbook -i $1.dev.wdevops.fun, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1  main.yml
